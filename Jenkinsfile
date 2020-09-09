1f2781be7d3d6d5dc620c2592420d1a8780f4014
Alexander Montgomery <evaderxander@gmail.com>
2020-09-09 01:19:29 -0400

    Init (#1)
    * initial commit

---
+++ Jenkinsfile
@@ -0,0 +1,56 @@
+podTemplate(yaml: """
+apiVersion: v1
+kind: Pod
+metadata:
+  labels:
+    some-label: builder
+spec:
+  containers:
+  - name: application-container
+    image: openjdk:14-alpine
+    command:
+    - cat
+    tty: true
+    volumeMounts:
+    - mountPath: '/workspace/opt/app/shared/'
+      name: sharedvolume
+  - name: kaniko
+    workingDir: /tmp/jenkins
+    image: gcr.io/kaniko-project/executor:debug-v0.14.0
+    imagePullPolicy: Always
+    capabilities:
+      add: ["IPC_LOCK"]
+    command:
+    - /busybox/cat
+    tty: true
+    volumeMounts:
+    - mountPath: '/workspace/opt/app/shared/'
+      name: sharedvolume
+  volumes:
+    - name: sharedvolume
+      emptyDir: {}
+"""
+) {
+   node(POD_LABEL) {
+    stage('Build and test') {
+     checkout scm
+     container('application-container') {
+      sh 'chmod 777 gradlew'
+      sh './gradlew clean build'
+      sh 'cp build/distributions/search-and-sip-boot-*-SNAPSHOT.zip /workspace/opt/app/shared/app.zip'
+      sh 'cp Dockerfile /workspace/opt/app/shared/'
+     }
+    }
+    stage('Build with Kaniko') {
+     environment {
+      PATH = "/busybox:/kaniko:$PATH"
+     }
+      container(name: 'kaniko', shell: '/busybox/sh') {
+       sh 'cp /workspace/opt/app/shared/* /workspace/'
+       sh 'pwd'
+       sh 'ulimit -n 10000'
+       sh '/kaniko/executor -f Dockerfile --destination=docker.ultimaengineering.io/search-and-sip-api:latest'
+      }
+     }
+   }
+  }
---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	application
+	id("org.springframework.boot") version "2.3.3.RELEASE"
+	id("io.spring.dependency-management") version "1.0.10.RELEASE"
+	kotlin("jvm") version "1.3.72"
+	kotlin("plugin.spring") version "1.3.72"
+}
+
+group = "com.ultimaengineering"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	maven(url = "https://nexus.ultimaengineering.io/repository/maven-central/")
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-elasticsearch")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("io.springfox:springfox-boot-starter:3.0.0")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+	testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
