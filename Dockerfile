71f596fc957a30282f0806c48df6d98018dac51a
Alexander Montgomery <evaderxander@gmail.com>
2021-04-18 16:28:39 -0400

    docker and repo additions.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM adoptopenjdk:11-jre-hotspot-bionic
 MAINTAINER Alexander Montgomery
 RUN mkdir /app
-COPY /workspace/app.jar /app/app.jar
+COPY app.jar /app/app.jar
 CMD java -jar /app/app.jar
--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
+	maven(url = "https://nexus.ultimaengineering.io/repository/maven-public/")
 	mavenCentral()
 }
 
